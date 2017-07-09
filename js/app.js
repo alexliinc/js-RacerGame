fce8b68091bbc7e4f042c3b7391ca2d5f769c332
alexliinc <alexli@indiana.edu>
2017-07-08 20:07:40 -0600

    basic racing game works

--- js/app.js
+++ js/app.js
@@ -1,41 +1,53 @@
+/*Project Name: Racer Game
+Author: Alex Li
+Developer @GA in DENVER */
+
 var playerOne = document.getElementById("player1");
-
 var playerTwo = document.getElementById("player2");
-
 var finish = document.getElementById("finishLine");
 
-var imageOffset = 0
+var finishPosition = finish.getBoundingClientRect();
 
-function movePlayer(player) {
-  imageOffset += 25
-  player.style.left = imageOffset + "px";
+var playerOneOffset = 0;
+var playerTwoOffset = 0;
+
+function movePlayer1(player) {
+  playerOneOffset += 25
+  player.style.left = playerOneOffset + "px";
 }
 
+function movePlayer2(player) {
+  playerTwoOffset += 25
+  player.style.left = playerTwoOffset + "px";
+}
 
 
 document.onkeydown = checkKey;
 
 function checkKey(e) {
     var playerOnePosition = playerOne.getBoundingClientRect();
-    var finshPosition = finish.getBoundingClientRect();
+    var playerTwoPosition = playerTwo.getBoundingClientRect();
+
     e = e || window.event;
 
     if (e.keyCode == '81') {
         console.log("q");
-        movePlayer(playerOne);
-        //console.log(playerOnePosition);
-        //console.log(finshPosition.left);
-        if (playerOnePosition.right>finshPosition.left){
-          alert("winner");
+        movePlayer1(playerOne);
+        console.log(playerOnePosition.right);
+        //console.log(finishPosition.left);
+        if (playerOnePosition.right>finishPosition.left){
+          alert("Player One is the winner");
         }
     }
     else if (e.keyCode == '80') {
         console.log("p");
-        movePlayer(playerTwo);
+        movePlayer2(playerTwo);
+        if (playerTwoPosition.right>finishPosition.left){
+          alert("Player TWO is the winner");
+        }
     }
-
 }
 
-function winner(){
+function winner(cur,finish){
 
 }
