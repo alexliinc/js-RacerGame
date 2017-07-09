9d653c888040a7dac7a941aa6b2e04110eb0ca57
alexliinc <alexli@indiana.edu>
2017-07-09 12:34:37 -0600

    edit text

--- js/app.js
+++ js/app.js
@@ -40,14 +40,14 @@ function checkKey(e) {
         console.log(playerOnePosition.right);
         //console.log(finishPosition.left);
         if (playerOnePosition.right>finishPosition.left){
-          alert("Player One is the winner");
+          alert("DareDevil is the winner");
         }
     }
     else if (e.keyCode == '80') {
         console.log("p");
         movePlayer2(playerTwo);
         if (playerTwoPosition.right>finishPosition.left){
-          alert("Player TWO is the winner");
+          alert("MegaMan that looks like DareDevil is the winner");
         }
     }
 }
